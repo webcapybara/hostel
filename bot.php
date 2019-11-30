cc1f30fb51b15dce101c0a18f9513e6b2677c25a
w1le <w1le@mail.ru>
2019-11-30 13:52:28 +0400

    upd

---
+++ bot.php
@@ -0,0 +1,41 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+//Переменная $name,$phone, $mail получает данные при помощи метода POST из формы
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+
+//в переменную $token нужно вставить токен, который нам прислал @botFather
+$token = "836163385:AAGVFkUpAw2F9XMvx-2SHH0NheOlQmQmwpU";
+
+//нужна вставить chat_id (Как получить chad id, читайте ниже)
+$chat_id = "-331898682";
+
+//Далее создаем переменную, в которую помещаем PHP массив
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: +' => $phone,
+  'Место заказа:' => $email
+);
+
+//При помощи цикла перебираем массив и помещаем переменную $txt текст из массива $arr
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+//Осуществляется отправка данных в переменной $sendToTelegram
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+//Если сообщение отправлено, напишет "Thank you", если нет - "Error"
+/* if ($sendToTelegram) {
+  echo "Thank you";
+} else {
+  echo "Error";
+} */
+
+header("Location:index.html");
+
+?>
--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function show(a) {
 }
 
 
+
 // переменные для слайдеров
 
 var swiper1 = new Swiper('.swiper1', {
@@ -75,7 +76,7 @@ var swiperhead = new Swiper('.swiper-head', {
     observer: true,
     observeParents: true,
     autoplay: {
-        delay: 2500,
+        delay: 10000,
         disableOnInteraction: false,
     },
     pagination: {
@@ -90,14 +91,139 @@ var swiperhead = new Swiper('.swiper_around', {
         delay: 10000,
         disableOnInteraction: false,
     },
-    
+
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
+    },
 });
 
+
+
 //плавное появление страницы
-setTimeout(function(){
+/* setTimeout(function(){
 	document.body.classList.add('body_visible');
-}, 100);
+}, 1000);
+ */
+
+//активный пункт меню
+
+
+
+
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 150, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+/* function activecor() {
+    'use strict';
+  
+    var section = document.querySelectorAll(".section");
+    var sections = {};
+    var i = 0;
+  
+    Array.prototype.forEach.call(section, function(e) {
+      sections[e.id] = e.offsetTop-200;
+      console.log(e.offsetTop);
+      
+    });
+  
+    window.onscroll = function() {
+      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  
+      for (i in sections) {
+        if (sections[i] <= scrollPosition) {
+          document.querySelector('.active').setAttribute('class', ' ');
+          document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+          
+        }
+      }
+    };
+    
+  };
+activecor.init(); */
+
+
+const modalCall = $("[data-modal]");
+const modalClose = $("[data-close]");
+
+modalCall.on("click", function (event) {
+    event.preventDefault();
+    let $this = $(this);
+    let modalId = $this.data('modal');
+    $(modalId).addClass('show');
+
+    $("body").addClass('no-scroll')
+
+
+    setTimeout(function () {
+        $(modalId).find(".modal__dialog").css({
+            transform: "rotateX(0)"
+        })
+    }, 200);
+
+
+});
+
+modalClose.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents('.modal');
+
+
+    modalParent.find(".modal__dialog").css({
+        transform: "rotateX(90deg)"
+    });
+    setTimeout(function () {
+        modalParent.removeClass('show');
+        $("body").removeClass('no-scroll');
+
+    }, 200);
+
+
+
+});
+
+$(".modal").on("click", function (event) {
+
+
+    let $this = $(this);
+
+    $this.find(".modal__dialog").css({
+        transform: "rotateX(90deg)"
+    });
+    setTimeout(function () {
+        $this.removeClass('show');
+        $("body").removeClass('no-scroll')
+
+    }, 200);
+
+});
+
+$(".modal__dialog").on("click", function (event) {
+event.stopPropagation();
+});
+
